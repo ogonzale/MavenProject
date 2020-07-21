8a5e8dc656e45570d3446e3eced11aa8800b16cc
ottoemilio <ottoemilio@gmail.com>
2020-07-21 23:36:51 +0200

    Added new files to the project

---
+++ src/main/java/it/enaip/corso/quickstart/App.java
@@ -0,0 +1,13 @@
+package it.enaip.corso.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
